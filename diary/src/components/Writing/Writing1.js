57e8d7680f81965199454f3c0e80dd8ce43f3b8f
parksujin <sherry5667@ewha.ac.kr>
2025-01-15 02:30:39 +0900

    일기 생성 페이지

---
+++ diary/src/components/Writing/Writing1.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import "./Writing1.css";
+import { Link } from "react-router-dom";
+import Writing from "./components/Contents/Writing";
+import Circle from "./components/Contents/Circle";
+import Square from "./components/Contents/Square";
+
+function Writing1() {
+  const [temp, setTemp] = useState(20);
+  const [diffX, setDiffX] = useState(200);
+
+  return (
+    <>
+      <Writing>
+        <div className="wr-row">
+          <Circle number="1" isActive={true} />
+          <Circle number="2" isActive={false} />
+        </div>
+        <h1 className="wr-h1">Q. 오늘 당신의 하루는 몇 도 인가요?</h1>
+        <h2 className="wr-h2">온도 커서를 움직여서 설정해요.</h2>
+        <div>
+          <div className="wr-row">
+            <Square color="#1343a9"/>
+            <Square color="#1A59A6"/>
+            <Square color="#226DA3"/>
+            <Square color="#267BA1"/>
+            <Square color="#479596"/>
+            <Square color="#83C282"/>
+            <Square color="#A3D977"/>
+            <Square color="#B7BF64"/>
+            <Square color="#D59A49"/> 
+            <Square color="#F3712D"/>
+            <Square color="#F65D36"/>
+            <Square color="#F94443"/>
+          </div>
+          <div
+            className="wr-cursor"
+            onPointerDown={(e) => {
+              const initX = e.clientX;
+              const dragMove = (e) => {
+                const newDiffX = diffX + e.clientX - initX;
+                const boundedDiffX = Math.max(-10, Math.min(410, newDiffX));
+                setDiffX(boundedDiffX);
+                const newTemp = Math.round(-9 + (boundedDiffX / 420) * 60);
+                setTemp(newTemp);
+              };
+              const dragEnd = () => {
+                document.removeEventListener("pointermove", dragMove);
+              };
+              document.addEventListener("pointermove", dragMove);
+              document.addEventListener("pointerup", dragEnd, { once: true });
+            }}
+            style={{ transform: `translateX(${diffX}px)` }}
+          ></div>
+        </div>
+        <h3 className="wr-h3">오늘은 {temp} ℃입니다.</h3>
+        <Link to="/writing2"><button className="wr-orange-button">다음</button></Link>
+      </Writing>
+    </>
+  );
+}
+
+export default Writing1;
---
+++ diary/src/components/Writing/Writing2.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import "./Writing2.css";
+import Paper from "./components/Contents/Paper";
+import Writing from "./components/Contents/Writing";
+import Square from "./components/Contents/Square";
+import Circle from "./components/Contents/Circle";
+
+function Writing2() {
+  const [selectedPaper, setSelectedPaper] = useState(null);
+  const [selectedPaperData, setSelectedPaperData] = useState({});
+
+  const data = [
+    {
+      img: <img src={require("./assets/book.png")} className="paper-img" />,
+      name: "일상일기",
+      line1: "오늘 하루 있었던 일들을",
+      line2: "자유롭게 기록하고",
+      line3: "마음껏 기록해 보세요!",
+    },
+    {
+      img: <img src={require("./assets/book2.png")} className="paper-img" />,
+      name: "질문일기",
+      line1: "오늘 하루, 궁금한 질문에 답해보세요.",
+      line2: "그 속에서 나만의 이야기를",
+      line3: "발견할 수 있을 거예요.",
+    },
+    {
+      img: (
+        <img
+          src={require("./assets/books.png")}
+          className="paper-img"
+          style={{ width: "115px", height: "80px" }}
+        />
+      ),
+      name: "일상일기 & 질문일기",
+      line1: "오늘의 생각을 일기로 자유롭게 적고,",
+      line2: "그와 함께 궁금한 질문에 답하며",
+      line3: "하루를 더욱 특별하게 만들어보세요.",
+    },
+  ];
+  return (
+    <div>
+      <Writing>
+        <div className="wr-row">
+          <Circle number="1" isActive={true} />
+          <Circle number="2" isActive={true} />
+        </div>
+        <h1 className="wr-h1">Q. 오늘은 어떤 일기를 작성하고 싶나요?</h1>
+        <div className="wr-row">
+          {data.map((item, index) => (
+            <Paper
+              key={index}
+              img={item.img}
+              name={item.name}
+              line1={item.line1}
+              line2={item.line2}
+              line3={item.line3}
+              isSelected={selectedPaper === index}
+              onClick={() => {
+                setSelectedPaper(index);
+                setSelectedPaperData(item);
+              }}
+            />
+          ))}
+        </div>
+        <button className="wr-orange-button">완료</button>
+      </Writing>
+      <Square />
+    </div>
+  );
+}
+
+export default Writing2;
